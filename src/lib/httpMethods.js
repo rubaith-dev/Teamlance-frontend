ca6c3e632e8cad19657f084999fa12b9a2045d1a
Rubaith Adnan <dev.rubaith@gmail.com>
2023-06-15 02:02:31 +0600

    Fix: http methods made reusable

---
+++ src/lib/httpMethods.js
@@ -0,0 +1,44 @@
+import axiosInstance from "./axiosInstance";
+import { toast } from "react-toastify";
+
+const postRequest = async (url, data) => {
+  try {
+    const response = await axiosInstance.post(url, data);
+    return response.data;
+  } catch (error) {
+    // Show error alert
+    toast(error.response.data.message);
+  }
+};
+
+const getRequest = async (url) => {
+  try {
+    const response = await axiosInstance.get(url);
+    return response.data;
+  } catch (error) {
+    // Show error alert
+    toast(error.response.data.message);
+  }
+};
+
+const putRequest = async (url, data) => {
+  try {
+    const response = await axiosInstance.put(url, data);
+    return response.data;
+  } catch (error) {
+    // Show error alert
+    toast(error.response.data.message);
+  }
+};
+
+const deleteRequest = async (url) => {
+  try {
+    const response = await axiosInstance.delete(url);
+    return response.data;
+  } catch (error) {
+    // Show error alert
+    toast(error.response.data.message);
+  }
+};
+
+export { getRequest, postRequest, deleteRequest, putRequest };
--- src/pages/component/Homepage/ManageCategory/index.jsx
+++ src/pages/component/Homepage/ManageCategory/index.jsx
@@ -4,18 +4,24 @@ import { useStateProvider } from "@/context/StateContext";
 import { useQueryClient } from "react-query";
 import Select from "react-select";
 import { useForm } from "react-hook-form";
-import axiosInstance from "@/lib/axiosInstance";
 import { toast } from "react-toastify";
 import { X } from "lucide-react";
 import ACTIONS from "@/context/Actions";
+import { deleteRequest, postRequest } from "@/lib/httpMethods";
 
 // Manage Category Modal, add and delete category
 const ManageCategory = () => {
+  // fetch the global state to dispatch actions and get states
   const [{ showManageCategoryModal }, dispatch] = useStateProvider();
+
+  // useQuery client will be used to caching data and handle smooth api calls
   const queryClient = useQueryClient();
   const categoryOptions = queryClient.getQueryData("categories");
+
+  // manage state of selected category
   const [selectedCategory, setSelectedCategory] = useState(null);
 
+  // use hook form to manage adding category form management
   const {
     handleSubmit,
     register,
@@ -23,47 +29,60 @@ const ManageCategory = () => {
     reset,
   } = useForm({ categoryName: "" });
 
+  // call to action when user click add category button
   const addCategory = async ({ categoryName }) => {
-    try {
-      const response = await axiosInstance.post("/categories/add-category", {
-        name: categoryName,
-      });
-      toast(response.data.message);
-      queryClient.invalidateQueries("fetch-categories");
-      reset();
-    } catch (err) {
-      toast(err.response.data.message);
-    }
+    // calling reusable post request from httpMethods
+    const response = await postRequest("/categories/add-category", {
+      name: categoryName,
+    });
+    // if success show toast
+    toast(response?.message);
+
+    // invalidate previous "fetch-categories" query to get updated categories with added category
+    queryClient.invalidateQueries("fetch-categories");
   };
 
+  // call to action when user click delete category button
   const deleteCategory = async () => {
-    try {
-      const response = await axiosInstance.delete(
-        `/categories/${selectedCategory.value}`
-      );
-      toast(response.data.message);
-      queryClient.invalidateQueries("fetch-categories");
-      setSelectedCategory(null);
-    } catch (err) {
-      toast(err.response.data.message);
-    }
+    // calling reusable delete request from httpMethods
+    const response = await deleteRequest(
+      `/categories/${selectedCategory.value}`
+    );
+
+    // if success show toast
+    toast(response?.message);
+
+    // invalidate previous "fetch-categories" query to get updated categories with deleted category
+    queryClient.invalidateQueries("fetch-categories");
+
+    // clear the selected category
+    setSelectedCategory(null);
   };
 
+  // call to action to close modal
   const closeModal = () => {
+    // dispatch action to reset
     dispatch({
       type: ACTIONS.RESET,
     });
+
+    //reset hook form
     reset();
+
+    // clear selected category
+    setSelectedCategory(null);
   };
 
   return (
     <Modal isOpen={showManageCategoryModal}>
       <div className="w-[450px] rounded-md p-5 bg-gray-100 shadow-2xl">
+        {/* title and close button */}
         <div className="flex w-full justify-end mb-3">
-            <p className="w-full text-center text-gray-500">Manage Categories</p>
+          <p className="w-full text-center text-gray-500">Manage Categories</p>
           <X className="cursor-pointer" onClick={() => closeModal()} />
         </div>
 
+        {/* Form to add category */}
         <form onSubmit={handleSubmit(addCategory)} noValidate>
           <input
             type="text"
@@ -81,7 +100,8 @@ const ManageCategory = () => {
             Add
           </button>
         </form>
-
+        
+        {/* Section to delete category */}
         <p className="text-gray-500 mt-6 mb-2">Select Category to Delete</p>
         <Select
           options={categoryOptions?.data}
